407e915bb122fa9780bb4a7a07e9840354473fd6
adnan922 <sofiadnan92@gmail.com>
2026-04-16 15:07:02 +0000

    added SpeedInsights

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ import Footer from '@/components/Footer';
 import CallButton from '@/components/CallButton';
 import WhatsAppButton from '@/components/WhatsAppButton';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Home() {
   return (
@@ -30,6 +31,7 @@ export default function Home() {
       <FAQ />
       <ContactSection />
       <Analytics />
+      <SpeedInsights />
       <PopularPlaces />
       <Footer />
       <CallButton />
